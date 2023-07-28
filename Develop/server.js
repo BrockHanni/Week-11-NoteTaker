f02eefbe91c0daf2c37e1969c420ae915a052422
BrockHanni <hannibr26@gmail.com>
2023-07-27 23:43:15 -0600

    cleaning files

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const database = require('./db/db.json');
 const readFileAsync = util.promisify(fs.readFile);
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+// use the public folder for static files, while encoding the url and json
 
+// routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
@@ -19,7 +21,6 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     readFileAsync('./db/db.json').then((data) => 
         res.json(JSON.parse(data)));
-    
 });
 
 app.post('/api/notes', (req, res) => {
@@ -37,13 +38,13 @@ app.get('*', (req, res) => {
 });
 // the get with the * needs to be the last get or it will override the other gets
 
-
+// uuid from unit 11 random id generator
 const uuid = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
   };
-
+// shows the new note in the db
 function newNoteDb(body, notesArray) {
     const newNotes = body;
     if (!Array.isArray(notesArray))
@@ -53,7 +54,7 @@ function newNoteDb(body, notesArray) {
     
     body.id = notesArray[0];
     body.id = uuid()
-// change with uuid from unit 11 random id generator
+    // change with uuid from unit 11 random id generator
     notesArray.push(newNotes);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
@@ -61,7 +62,7 @@ function newNoteDb(body, notesArray) {
     );
     return newNotes;
 }
-
+// removes the note from the db
 function removeNoteDb(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
